cff8612ee78fb090b73665ac742e3aaca7b27653
Steve <stephenkoller@gmail.com>
2013-10-16 16:54:05 -0400

    PSC-1 Added Giles, NUnit, Should assertions, and started tests and code for authentication

--- PcaSoundCloud.API.Tests.Integration/Class1.cs
+++ PcaSoundCloud.API.Tests.Integration/Class1.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace PcaSoundCloud.API.Tests.Integration
 {
-    public class Class1
-    {
+    public class AuthenticationTests
+    {  
+        
+        [Test]
+        public void ApiShouldConnect()
+        {
+            var sut = new NSoundCloud();
+            var actual = sut.Authentication();
+
+
+            //_sut.GetClientId().ShouldReturn("873cc698f936328f3b702353621a5e93");
+        }
     }
 }
---
+++ PcaSoundCloud.API/NSoundCloud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcaSoundCloud.API
+{
+    public class NSoundCloud
+    {
+        //private string client_id { get; set; } //= "873cc698f936328f3b702353621a5e93";
+        //private const string client_secret = "fe2a764781dff6d5c65add05101a005d";
+        //https://soundcloud.com/connect
+        //https://api.soundcloud.com/oauth2/token
+        public void GetClientId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
