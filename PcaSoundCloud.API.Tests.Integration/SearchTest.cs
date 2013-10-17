e791026ab1fa8bb323758e5894dfc1b472751ddb
Adam Gartee <adam@gartee.net>
2013-10-17 19:49:01 -0400

    Search in progress

---
+++ PcaSoundCloud.API.Tests.Integration/SearchTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PcaSoundCloud.Shared.Entities;
+using RestSharp;
+
+namespace PcaSoundCloud.API.Tests.Integration
+{
+    [TestFixture]
+    public class SearchTest
+    {
+        private RestClient _client;
+
+        // https://api.soundcloud.com/tracks.format?consumer_key=apigee&filter=all&order=created_at&limit=3&username=Vindata
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = new RestClient("https://api.soundcloud.com/");
+        }
+
+        [Test]
+        public void ConstructValidURL()
+        {
+            var request = new RestRequest("tracks.format", Method.GET);
+            request.AddParameter("consumer_key", "apigee");
+            request.AddParameter("filter", "all");
+            request.AddParameter("limit", "1");
+
+            var response = _client.Execute(request);
+
+            Assert.That(response.ResponseUri.ToString(), Is.EqualTo(
+                "https://api.soundcloud.com/tracks.format?consumer_key=apigee&filter=all&limit=1"));
+        }
+
+        [Test]
+        public void CanSearch()
+        {
+            var request = new RestRequest("tracks.format", Method.GET);
+            request.AddParameter("consumer_key", "apigee");
+            request.AddParameter("filter", "all");
+            request.AddParameter("limit", "1");
+
+            var response = _client.Execute(request);
+
+            var content = response.Content;
+            Assert.That(content.Contains("404 - Not Found"), Is.EqualTo(false));
+            Assert.That(content.Contains("401 - Unauthorized"), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void CanDeserialize()
+        {
+            var request = new RestRequest("tracks.format", Method.GET);
+            request.AddParameter("consumer_key", "apigee");
+            request.AddParameter("filter", "all");
+            request.AddParameter("limit", "1");
+
+            var response = _client.Execute<track>(request);
+
+            var data = response.Data;
+            Assert.That(data.id, Is.Not.Null);
+        }
+
+
+        
+
+
+    }
+}
---
+++ PcaSoundCloud.Shared/Entities/track.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcaSoundCloud.Shared.Entities
+{
+    public class track
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string createdat { get; set; }
+        public string userid { get; set; }
+        public string duration { get; set; }
+        public string commentable { get; set; }
+        public string state { get; set; }
+        public string originalcontentsize { get; set; }
+        public string sharing { get; set; }
+        public string taglist { get; set; }
+        public string permalink { get; set; }
+        public string streamable { get; set; }
+        public string embeddableby { get; set; }
+        public string downloadable { get; set; }
+        public string purchaseurl { get; set; }
+        public string labelid { get; set; }
+        public string purchasetitle { get; set; }
+        public string genre { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string labelname { get; set; }
+        public string release { get; set; }
+        public string tracktype { get; set; }
+        public string keysignature { get; set; }
+        public string isrc { get; set; }
+        public string videourl { get; set; }
+        public string bpm { get; set; }
+        public string releaseyear { get; set; }
+        public string releasemonth { get; set; }
+        public string releaseday { get; set; }
+        public string originalformat { get; set; }
+        public string license { get; set; }
+        public string uri { get; set; }
+        public string user { get; set; }
+        public string permalinkurl { get; set; }
+        public string artworkurl { get; set; }
+        public string waveformurl { get; set; }
+        public string streamurl { get; set; }
+        public string playbackcount { get; set; }
+        public string downloadcount { get; set; }
+        public string favoritingscount { get; set; }
+        public string commentcount { get; set; }
+        public string attachmentsuri { get; set; }
+    }
+}
