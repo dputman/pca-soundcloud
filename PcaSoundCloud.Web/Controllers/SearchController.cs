7cf59fff7e8005e3316f0529503e5b88287696fc
unknown <benjaminstrawn@gmail.com>
2013-10-17 19:52:37 -0400

    added searchController and angular view

---
+++ PcaSoundCloud.Web/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PcaSoundCloud.Shared;
+
+namespace PcaSoundCloud.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ITrackService _service;
+
+        public SearchController(ITrackService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult searchMusic(string searchString)
+        {
+            var searchResults = _service.Search(searchString);
+            return Json(new {search = searchString}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
