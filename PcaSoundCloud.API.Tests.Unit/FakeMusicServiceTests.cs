aeed8cb39e4a5c71e900416df5a0139e5dc8103c
detroitpro <detroitpro@gmail.com>
2013-10-22 18:50:24 -0400

    fake music service

---
+++ PcaSoundCloud.API.Tests.Unit/FakeMusicServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using PcaSoundCloud.Shared.Entities;
+using RestSharp;
+
+namespace PcaSoundCloud.API.Tests.Unit
+{
+    public class FakeMusicServiceTests
+    {
+        [Test]
+        public void Return_data()
+        {
+            var service = new FakeMusicService();
+            var data = service.CallMusicService<Track>(new RestRequest());
+            data.Should().NotBe(null);
+            data.GetType().Should().Be(typeof(Track));
+        } 
+    }
+}
--- PcaSoundCloud.API/IMusicService.cs
+++ PcaSoundCloud.API/IMusicService.cs
@@ -4,8 +4,22 @@ namespace PcaSoundCloud.API
 {
     public interface IMusicService
     {
-        
         void SetApiKey(string apiKey);
         T CallMusicService<T>(RestRequest request) where T : new();
+    }
+
+
+    public class FakeMusicService : IMusicService
+    {
+        public void SetApiKey(string apiKey)
+        {
+            //thanks. I have an API key now.
+        }
+
+        public T CallMusicService<T>(RestRequest request) where T : new()
+        {
+            //this would call the API and return data of type T.
+            return new T();
+        }
     }
 }
