a4bfaeab7e0bc248f7a444d83e27bb0def694e08
jeff5048 <jeff5048@aol.com>
2013-11-05 19:58:47 -0500

    Add DaveController and Tests

---
+++ PcaSoundCloud.Web/Controllers/DaveController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Web;
+using System.Web.Mvc;
+using PcaSoundCloud.Core;
+using PcaSoundCloud.Web.Models;
+
+namespace PcaSoundCloud.Web.Controllers
+{
+    public class DaveController : Controller
+    {
+        //
+        // GET: /Dave/
+
+        public ActionResult Index(int userId)
+        {
+	        var favorites = new TrackService().GetFavoriteTracksByUserId(userId);
+	        var model = new UserModel {Favorites = favorites};
+            return View(model);
+        }
+    }
+}
---
+++ PcaSoundCloud.Web/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PcaSoundCloud.Shared.Entities;
+
+namespace PcaSoundCloud.Web.Models
+{
+	public class UserModel
+	{
+		 public IEnumerable<Track> Favorites { get; set; } 
+	}
+}
