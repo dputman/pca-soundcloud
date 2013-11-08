36bf5230e2e4842b6634bb777e1be563b2aedfa9
Steve <stephenkoller@gmail.com>
2013-11-07 19:03:02 -0500

    Renamed HomeController to LoginController and UserController to HomeController

--- PcaSoundCloud.Web/Controllers/HomeController.cs
+++ PcaSoundCloud.Web/Controllers/HomeController.cs
@@ -1,15 +1,22 @@
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using PcaSoundCloud.API;
 
 namespace PcaSoundCloud.Web.Controllers
 {
     public class HomeController : DefaultController
     {
         //
-        // GET: /Home/
+        // GET: /User/
 
-        public ActionResult Index()
+        public ActionResult Index(string access_token, string scope)
         {
-            return View();
+            this.HttpContext.Session.Add("token", access_token);
+            var _service = new UserApi(new MusicService());
+            var me = _service.GetUserByAccessToken(access_token);
+            this.HttpContext.Session.Add("username", me.username);
+            return View(me);
         }
     }
 }
---
+++ PcaSoundCloud.Web/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+using System.Web.Mvc;
+
+namespace PcaSoundCloud.Web.Controllers
+{
+    public class LoginController : DefaultController
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
